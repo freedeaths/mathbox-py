6e5c3499fccf188568caba0c8de1928fa796dc0d
freedeaths <register917@gmail.com>
2021-12-19 00:00:19 +0800

    initialize the repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='mathbox',
+    python_requires='>=3.8.0',
+    version='0.0.1',
+    description="A math toolbox.",
+    author='freedeaths',
+    author_email='register917@gmail.com',
+    url='https://github.com/freedeaths/mathbox-py.git',
+    packages=['mathbox'],
+    install_requires=[],
+    include_package_data=True,
+    license="MIT",
+    keywords=['math', 'data-processing'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
